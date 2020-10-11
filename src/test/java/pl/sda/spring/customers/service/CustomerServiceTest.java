a7469052b3134f0f0d2fc2bfbf183219ef8d579d
Kamil Jasek <kamil.jasek@dbschenker.com>
2020-10-11 14:00:43 +0200

    spring core 3

--- src/test/java/pl/sda/spring/customers/service/CustomerServiceTest.java
+++ src/test/java/pl/sda/spring/customers/service/CustomerServiceTest.java
@@ -1,6 +1,8 @@
 package pl.sda.spring.customers.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +14,16 @@ class CustomerServiceTest {
     @Autowired
     private CustomerService customerService;
 
+    @Autowired
+    private CustomerService secondCustomerService;
+
     @Test
     void testCreateCustomer() {
         assertNotNull(customerService);
+    }
+
+    @Test
+    void testIsSingleton() {
+        assertEquals(customerService, secondCustomerService);
     }
 }
